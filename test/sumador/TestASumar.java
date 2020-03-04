540af07cecd074818bd59c2238d541a2940443ba
Jordi Codevila de León <jordicodeviladl@gmail.com>
2020-03-04 15:27:59 +0100

    Cuando el parámetro es un valor negativo.

--- test/sumador/TestASumar.java
+++ test/sumador/TestASumar.java
@@ -4,7 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * @author Jordi
+ *
+ */
 public class TestASumar {
 
-
+	/**
+	 * si es negativo el espacio queda vacío
+	 */
+	@Test
+	public void testNegativo() {
+		ASumar sumi = new ASumar("-1");
+		String resultado = sumi.mostrar();
+		assertEquals("-1", resultado);
+		}
+	
 }
