6fe527051a72a0ad2b784b5211289bc156fdc16d
Jordi Codevila de León <jordicodeviladl@gmail.com>
2020-03-04 15:22:12 +0100

    He creado la primera prueba y ha sido valida

--- test/sumador/TestASumar.java
+++ test/sumador/TestASumar.java
@@ -4,7 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * @author Jordi
+ *
+ */
 public class TestASumar {
 
-
+	/**
+	 * Si el resultado es = 7 sera valida 
+	 */
+	@Test
+	public void testUnaCifra() {
+		ASumar sumi = new ASumar("7");
+		String resultado = sumi.mostrar();
+		assertEquals("7=7",resultado);
+	}
+		
 }
